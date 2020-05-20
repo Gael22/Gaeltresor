2410a86a7eb2871dafa95b77967e64e5866fd3b8
Gael Gohoungo <gohoungogael@gmail.com>
2020-05-19 22:05:33 -0400

    modified contact.Js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-    <h3><span>I'm a {city} based {occupation}. {description}.</span></h3>
+    <h3><span>I'm an {city} based {occupation}. {description}.</span></h3>
             <hr />
             <ul className="social">
                {networks}
